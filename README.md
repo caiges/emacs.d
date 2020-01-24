4a89340a26a63fbbd97671c9b58c43ddbb1585e3
caiges <caigesn@gmail.com>
2020-01-23 20:53:34 -0700

    Switch to doom theme and add some other niceties

---
+++ README.md
@@ -0,0 +1,10 @@
+# Overview
+
+Clone this repo into `~/.emacs.d`.
+
+# All The Icons
+
+The all the icons package is installed but the fonts probably are not. To install them:
+
+`M-x all-the-icons-install-fonts`
+
--- init.el
+++ init.el
@@ -74,9 +74,35 @@
 
 ;; Fire up the minor modes the theme we want going all the time everywhere
 (require 'use-package)
-(use-package spacemacs-common
-    :ensure spacemacs-theme
-    :config (load-theme 'spacemacs-dark t))
+
+(use-package all-the-icons
+  :ensure t)
+
+(use-package neotree
+  :ensure t
+  :config
+  (global-set-key [f8] 'neotree-toggle)
+)
+
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-city-lights t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
+  (doom-themes-treemacs-config)
+  
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
 
 (use-package ace-window
   :bind (
